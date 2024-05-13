fc7f6bef2084ba25fe4ed4ed84ef18be02888beb
nishitnalinsrivastava <nnalin04@gmail.com>
2024-05-13 19:12:05 +0530

    Updated the naming for rest endpoint.

--- src/main/java/com/collector/collectorbackend/controller/CollectorController.java
+++ src/main/java/com/collector/collectorbackend/controller/CollectorController.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("collecter")
+@RequestMapping("collector")
 @RequiredArgsConstructor
 public class CollectorController {
 
