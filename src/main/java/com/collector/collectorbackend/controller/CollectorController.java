f39716729455784910fef4424ef8eb670c4066e3
nishitnalinsrivastava <nnalin04@gmail.com>
2024-05-14 21:59:49 +0530

    Added CrossOrigin.

--- src/main/java/com/collector/collectorbackend/controller/CollectorController.java
+++ src/main/java/com/collector/collectorbackend/controller/CollectorController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("collector")
 @RequiredArgsConstructor
